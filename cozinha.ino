7029fb3758f62956f3e46548ccfc7770cfa4302f
BeaLimaS <150667191+BeaLimaS@users.noreply.github.com>
2023-11-14 23:42:57 +0000

    Update cozinha.ino

--- cozinha.ino
+++ cozinha.ino
@@ -2,21 +2,21 @@
 
 //variables for the State Machine
 bool lastState = 0;  //last state of the terminal
-bool currentState;     //current state of the terminal
 
+enum States{ACEITA, PREPARAR, PRONTO, ESPERA};
+States currentState = ESPERA;
+bool buttonPressed = 0;
 
 void detectPress(){ //function to detect if the button is pressed
   bool currentState = digitalRead(buttonPin);
   if((lastState == LOW) && (currentState == HIGH)){
-    buttonPressed = true;
+    buttonPressed = 1;
   }
   else {
-    buttonPressed = false;
+    buttonPressed = 0;
   }
   lastState = currentState;
 }
-
-void 
 
 void setup() {
   Serial.begin(9600);
@@ -25,6 +25,29 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switch (currentState){
+      case ESPERA:{
+        //FALTA METER O QUE O CLIENTE VAI RECEBER
+        //ou seja, vai desligar tudo o que estava ligado (entra em modo de espera)
+        if(detectPress){currentState = ACEITA;}
+      }
 
+      case ACEITA:{
+        //FALTA METER O QUE O CLIENTE VAI RECEBER
+        //ou seja, liga 2 primeiros LEDs do NeoPixel; Som de início
+        if(detectPress){currentState = PREPARAR;}
+      }
+
+      case PREPARAR:{
+        //FALTA METER O QUE O CLIENTE VAI RECEBER
+        //ou seja, passar imagens no TFT; loading atraves dos NeoPixeis
+        if(detectPress){currentState = PRONTO;}
+      }
+
+      case PRONTO:{
+        //FALTA METER O QUE O CLIENTE VAI RECEBER
+        //ou seja, Piezzo toca; NeoPixeis piscam; TFT mostra imagem de pronto
+        if(detectPress){currentState = ESPERA;}
+      }
+  }
 }
