c16c5e8eff5e69735d37ac2fe50085cedaeb875b
dianagerardo8 <150667797+dianagerardo8@users.noreply.github.com>
2023-12-08 00:10:12 +0000

    Update cozinha.ino

--- cozinha.ino
+++ cozinha.ino
@@ -1,22 +1,43 @@
+#include <Wire.h>
 #include <WiFi.h>
-#include<SPI.h>
 
-char ssid[] = "myNetwork";          //  your network SSID (name)
-char pass[] = "myPassword";   // your network password
+#define buttonPin 27
 
-int status = WL_IDLE_STATUS;
-char servername[]="google.com";  // remote server we will connect to
+const char *ssid = "Cozinha";
+const char *password = "123456789";
+const uint16_t port = 80;
 
 WiFiClient client;
 
-void setup() {
-  delay(5000);
-  Serial.begin(9600);
-  Serial.println("Attempting to connect to WPA network...");
-  Serial.print("SSID: ");
-  Serial.println(ssid);
+enum State {
+  ESPERA,
+  ACEITE,
+  PREPARAR,
+  PRONTO
+};
 
-  status = WiFi.begin(ssid, pass);
+State currentState = ACEITE ;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;  //debounce time in milliseconds
+
+void sendCommand() {
+  if (client.connected()) {
+    client.print(static_cast<int>(currentState));
+
+    Serial.print("Sent command to client: ");
+    Serial.println(currentState);
+    delay(1000);
+  }
+}
+
+IPAddress server_ip(192, 168, 1, 1);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  // Connect to WiFi
+   int status = WiFi.begin(ssid, password);
   if ( status != WL_CONNECTED) {
     Serial.println("Couldn't get a wifi connection");
     // don't do anything else:
@@ -26,15 +47,46 @@ void setup() {
     Serial.println("Connected to wifi");
     Serial.println("\nStarting connection...");
     // if you get a connection, report back via serial:
-    if (client.connect(servername, 80)) {
-      Serial.println("connected");
-      // Make a HTTP request:
-      client.println("GET /search?q=arduino HTTP/1.0");
-      client.println();
-    }
-  }
+}
 }
 
+
 void loop() {
-  Serial.println("Fuck...");
+  if (!client.connect(server_ip , 80)) { return; }
+
+  int buttonState = !digitalRead(buttonPin);
+
+  // Try to connect to the client
+ // if (!client.connected()) {
+   // client.connect("esp32-client", port);
+    //Serial.println("Not connected to client");
+  //}
+  Serial.println("VGJHVGJHKHUHB J");
+  client.println("PREPARAR");
+
+  switch (currentState) {
+    case ESPERA:
+      client.println("ESPERA");
+      currentState = ACEITE;
+      break;
+
+    case ACEITE:
+      client.println("ACEITE");
+      currentState = PREPARAR;
+      break;
+
+    case PREPARAR:
+      client.println("PREPARAR");
+      currentState = PRONTO;
+      break;
+
+    case PRONTO:
+      client.println("PRONTO");
+      currentState = ESPERA;  // Volta ao estado inicial para reiniciar o ciclo
+      break;
+
+    default:
+      // Tratamento de erro, se necessário
+      break;
+  }
 }
